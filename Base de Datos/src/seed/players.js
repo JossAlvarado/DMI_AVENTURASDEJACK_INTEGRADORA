2e8311cfc8c77081f686ccfbfce3e36d2431c767
JossAlvardao <josseline.alvaradovargas@utxicotepec.edu.mx>
2023-12-01 13:41:23 -0600

    Base de datos 18

---
+++ Base de Datos/src/seed/players.js
@@ -0,0 +1,174 @@
+const players = [
+    {
+        "name": "Eliana",
+        "email": "starchy9295@outlook.com",
+        "password": "12345",
+        "nickname": "artoria",
+        "birthdate": "2002-05-05",
+        "portrait_img": "uwu.png"
+    },
+    {
+        "name": "Elisabeth",
+        "email": "elisa@outlook.com",
+        "password": "12345",
+        "nickname": "Elisama11",
+        "birthdate": "2001-04-03",
+        "portrait_img": "left.png"
+    },
+    {
+        "name": "Jessica",
+        "email": "john@gmail.com",
+        "password": "password123",
+        "nickname": "johnny",
+        "birthdate": "1995-12-15",
+        "portrait_img": "john.png"
+    },
+    {
+        "name": "Alicia",
+        "email": "alice@yahoo.com",
+        "password": "securepass",
+        "nickname": "wonderalice",
+        "birthdate": "1990-08-22",
+        "portrait_img": "alice.jpg"
+    },
+    {
+        "name": "Bianca",
+        "email": "bob@hotmail.com",
+        "password": "bob123",
+        "nickname": "bobster",
+        "birthdate": "1988-03-10",
+        "portrait_img": "bob.png"
+    },
+    {
+        "name": "Sofia",
+        "email": "sara@gmail.com",
+        "password": "sara567",
+        "nickname": "saraberry",
+        "birthdate": "1993-06-27",
+        "portrait_img": "sara.png"
+    },
+    {
+        "name": "Daniela",
+        "email": "daniel@yahoo.com",
+        "password": "daniel456",
+        "nickname": "danman",
+        "birthdate": "1987-11-18",
+        "portrait_img": "daniel.jpg"
+    },
+    {
+        "name": "Sophia",
+        "email": "sophie@hotmail.com",
+        "password": "sophie789",
+        "nickname": "sophster",
+        "birthdate": "1998-09-03",
+        "portrait_img": "sophie.png"
+    },
+    {
+        "name": "Michelle",
+        "email": "michael@gmail.com",
+        "password": "mikepass",
+        "nickname": "miketheman",
+        "birthdate": "1985-02-14",
+        "portrait_img": "michael.jpg"
+    },
+    {
+        "name": "Lillian",
+        "email": "lily@yahoo.com",
+        "password": "lilypass",
+        "nickname": "lilyflower",
+        "birthdate": "1996-07-08",
+        "portrait_img": "lily.png"
+    },
+    {
+        "name": "Olivia",
+        "email": "oliver@gmail.com",
+        "password": "oliver123",
+        "nickname": "olivero",
+        "birthdate": "1994-10-20",
+        "portrait_img": "oliver.png"
+    },
+    {
+        "name": "Isabella",
+        "email": "isabella@example.com",
+        "password": "password123",
+        "nickname": "bella21",
+        "birthdate": "1998-03-10",
+        "portrait_img": "isabella.png"
+    },
+    {
+        "name": "Sophie",
+        "email": "sophie@example.com",
+        "password": "sophie567",
+        "nickname": "sophiemarie",
+        "birthdate": "1990-12-15",
+        "portrait_img": "sophie.jpg"
+    },
+    {
+        "name": "Emma",
+        "email": "emma@example.com",
+        "password": "emmapass",
+        "nickname": "emmalicious",
+        "birthdate": "1985-07-22",
+        "portrait_img": "emma.png"
+    },
+    {
+        "name": "Ava",
+        "email": "ava@example.com",
+        "password": "avasecure",
+        "nickname": "avadreamer",
+        "birthdate": "1995-04-03",
+        "portrait_img": "ava.jpg"
+    },
+    {
+        "name": "Grace",
+        "email": "grace@example.com",
+        "password": "grace123",
+        "nickname": "graceful",
+        "birthdate": "1993-09-18",
+        "portrait_img": "grace.png"
+    },
+    {
+        "name": "Lily",
+        "email": "lily@example.com",
+        "password": "lilypassion",
+        "nickname": "lilylove",
+        "birthdate": "1992-06-27",
+        "portrait_img": "lily.jpg"
+    },
+    {
+        "name": "Zoe",
+        "email": "zoe@example.com",
+        "password": "zoe456",
+        "nickname": "zoefly",
+        "birthdate": "1989-11-05",
+        "portrait_img": "zoe.png"
+    },
+    {
+        "name": "Chloe",
+        "email": "chloe@example.com",
+        "password": "chloe789",
+        "nickname": "chloestar",
+        "birthdate": "1991-08-30",
+        "portrait_img": "chloe.png"
+    },
+    {
+        "name": "Ella",
+        "email": "ella@example.com",
+        "password": "ellapass",
+        "nickname": "ellabella",
+        "birthdate": "1987-02-14",
+        "portrait_img": "ella.png"
+    },
+    {
+        "name": "Mia",
+        "email": "mia@example.com",
+        "password": "miapassword",
+        "nickname": "miamagic",
+        "birthdate": "1996-05-08",
+        "portrait_img": "mia.png"
+    }
+    
+    
+
+]
+export default players
